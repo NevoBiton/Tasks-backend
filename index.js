3a28c5ca146a1bc18e2dd136453589b8e3d98cb5
Nevo <bitonnevo@gmail.com>
2024-07-08 16:04:21 +0300

    path commit

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const path = require("path");
 const dotenv = require("dotenv");
 const cors = require("cors");
 
@@ -34,6 +35,9 @@ const userRoutes = require("./routes/user.route");
 app.use("/api/auth", authRoutes);
 app.use("/api/user", verifyToken, userRoutes);
 
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "public", "index.html"));
+});
 
 app.listen(PORT , () => {
     console.log(`Server is running on port ${PORT}`);
